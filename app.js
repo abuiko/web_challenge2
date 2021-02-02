bb5da39cd7d36aa2c1084db5b80b5264b8361bc6
Alena Buiko <abuiko.ma@gmail.com>
2021-02-02 11:25:10 -0500

    toggle burger menu on click

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const dots = document.querySelectorAll('.dot');
+// const dots = document.querySelectorAll('.dot');
 
-dots.forEach(dot => {
-    dot.addEventListener('click', () => {
-        dot.classList.add('active');
-    });
+// dots.forEach(dot => {
+//     dot.addEventListener('click', () => {
+//         dot.classList.add('active');
+//     });
+// })
+
+
+const burger = document.querySelector('.header__toggle');
+const nav = document.querySelector('.burger__menu');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    if (nav.classList.contains('open')) {
+        overlay.style.opacity = 1;
+        body.classList.add('no-scroll');
+
+    } else {
+        overlay.style.opacity = 0;
+        body.classList.remove('no-scroll')
+    }
 })
